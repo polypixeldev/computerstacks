bfaccd941e49dfeb52b0cc602c2bd1d730bbdc67
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-09 18:45:54 -0400

    Run prettier

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Community Project</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Community Project</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+		</>
+	);
 }
