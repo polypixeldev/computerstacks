02fc12ccbb432283c1f40510ed9c37b222c70620
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-10 14:44:51 -0400

    Custom errors

---
+++ app/pages/404.js
@@ -0,0 +1,11 @@
+function error404() {
+	return (
+		<main>
+			<section className="top section1">
+				<code className="bigCode">404 Not Found</code>
+			</section>
+		</main>
+	)
+}
+
+export default error404;
