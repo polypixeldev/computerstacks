d8a5efd91da25a6f01bee53b39723a6799bbeb67
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-10 14:44:27 -0400

    Main app wrapper

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,36 @@
 import "../styles/globals.css";
+import Head from "next/head";
+import MenuButton from "../public/menuButton.png";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<div>
+			<Head>
+				<title>Community Project</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<nav>
+				<div className="imageWrap">
+					<Image src={MenuButton} alt="menu button" layout="fill" objectFit="contain" objectPosition="25% 50%" />
+				</div>
+				<h1>Community Project</h1>
+			</nav>
+			<Component {...pageProps} />
+			<footer>
+				<h2>Community Project</h2>
+				<p><strong>© 2021, Samuel Fernandez</strong></p>
+				<div>
+					<h3>Category</h3>
+					<div>
+						<p>Link</p>
+						<p>Link</p>
+						<p>Link</p>
+					</div>
+				</div>
+			</footer>
+		</div>
+	);
 }
 
 export default MyApp;
---
+++ app/pages/_document.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Zilla+Slab&family=Dosis:wght@300&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
