3378fa882d021f3a2e6f7aa95f32fd251fb5f4a7
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-11 17:36:36 -0400

    Main library page

---
+++ app/pages/library.js
@@ -0,0 +1,29 @@
+import HeadStyle from '../styles/Head.module.css';
+
+function library() {
+	return (
+		<main>
+			<section className={HeadStyle.head} id="head">
+				<h2>Resource Library</h2>
+				<p>Browse foo resources in bar subjects</p>
+			</section>
+			<section className="section1">
+				<div>
+					<p>1</p>
+				</div>
+			</section>
+			<section className="section2">
+				<div>
+					<p>2</p>
+				</div>
+			</section>
+			<section className="section3">
+				<div>
+					<p>3</p>
+				</div>
+			</section>
+		</main>
+	)
+}
+
+export default library;
