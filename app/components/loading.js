c5ae0280ef5d6366e3924a7f7b8d5dcc40418ff4
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-13 19:52:10 -0400

    Add mongodb adapter

---
+++ app/components/loading.js
@@ -0,0 +1,11 @@
+function Loading() {
+	return (
+		<main>
+			<section className="section1">
+				<h2>Loading...</h2>
+			</section>
+		</main>
+	)
+}
+
+export default Loading;
--- app/pages/api/auth/[...nextauth].js
+++ app/pages/api/auth/[...nextauth].js
@@ -1,16 +1,21 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb";
 
-const options = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+const handler = async (req, res) => {
+  return await NextAuth(req, res, {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+      }),
+    ],
+    adapter: MongoDBAdapter({
+      db: (await clientPromise).db("project")
     }),
-  ],
-  debug: false
+    debug: false
+  });
 }
-
-const handler = (req, res) => NextAuth(req, res, options);
 
 export default handler;
