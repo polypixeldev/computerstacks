fb866ae48157b1bfc432bbea395aa1e313165e78
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-13 17:29:25 -0400

    Add links & discover section to /roadmaps

--- app/pages/roadmaps.js
+++ app/pages/roadmaps.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import HeadStyles from "../styles/Head.module.css";
 
 function Roadmaps() {
@@ -8,8 +9,12 @@ function Roadmaps() {
 				<p>Your new helper for learning what you need to learn</p>
 			</section>
 			<section className="section1">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+				<button className="button"><Link href="/roadmaps/discover"><a className="link">Discover your roadmap</a></Link></button>
+			</section>
+			<section className="section2">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-				<button className="button">Explore now</button>
+				<button className="button"><Link href="/roadmaps/explore"><a className="link">Explore now</a></Link></button>
 			</section>
 		</main>
 	)
