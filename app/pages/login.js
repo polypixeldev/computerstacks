87e5b268be248ee254d678794782c1ffc7c46214
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-13 20:17:28 -0400

    Add sign in with google to login

--- app/pages/login.js
+++ app/pages/login.js
@@ -1,10 +1,25 @@
 import FormStyle from "../styles/Form.module.css";
+import HeadStyles from "../styles/Head.module.css";
+import LoginStyles from "../styles/Login.module.css";
+import { signIn } from "next-auth/react";
 
 function Login() {
+	function google(){
+		signIn('google', { callbackUrl: "http://localhost/dashboard"})
+	}
+
 	return (
 		<main>
 			<section className="section1">
 				<h2>Login</h2>
+				<br />
+				<button onClick={google} className={`button ${LoginStyles.providerButton}`}>Sign in with Google</button>
+				<div className={HeadStyles.actionDiv}>
+					<hr />
+					<p>OR</p>
+					<hr/>
+				</div>
+				<h3>Sign in with email</h3>
 				<form className={FormStyle.form}>
 					<label className={FormStyle.label}>
 						Username
