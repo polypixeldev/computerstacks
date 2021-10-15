da2dd52d94602036fff0230d0761e010f0ed2d68
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-14 20:47:11 -0400

    Add credential auth login

--- app/pages/api/auth/[...nextauth].js
+++ app/pages/api/auth/[...nextauth].js
@@ -1,19 +1,54 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
+import bcrypt from 'bcryptjs';
 
-const handler = async (req, res) => {
+async function handler(req, res) {
   return await NextAuth(req, res, {
     providers: [
       GoogleProvider({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET
       }),
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          username: { label: "Username", type: "text" },
+          password: { label: "Password", type: "password"}
+        },
+        async authorize(credentials, req){
+          const client = await clientPromise;
+          const db = client.db("project");
+          const users = db.collection("users");
+
+          const user = await users.findOne({ username: credentials.username });
+          if(!user) return null;
+          const auth = bcrypt.compareSync(credentials.password, user.password);
+
+          console.log(auth)
+          if(!auth){
+            return null;
+          } else {
+            delete user.password;
+            return user;
+          }
+        }
+      })
     ],
     adapter: MongoDBAdapter({
       db: (await clientPromise).db("project")
     }),
+    session: {
+      jwt: true
+    },
+    pages: {
+      signIn: "/login"
+    },
+    jwt: {
+      signingKey: process.env.JWT_SIGNING_PRIVATE_KEY
+    },
     debug: false
   });
 }
--- app/pages/login.js
+++ app/pages/login.js
@@ -1,11 +1,40 @@
 import FormStyle from "../styles/Form.module.css";
 import HeadStyles from "../styles/Head.module.css";
 import LoginStyles from "../styles/Login.module.css";
-import { signIn } from "next-auth/react";
+import { signIn, getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useState } from 'react';
 
 function Login() {
+	const router = useRouter();
+
+	const [ username, setUsername ] = useState('');
+	const [ password, setPassword ] = useState('');
+
 	function google(){
 		signIn('google', { callbackUrl: "http://localhost/dashboard"})
+	}
+
+	async function credentials(event){
+		event.preventDefault();
+		const res = await signIn('credentials', { username: username, password: password, redirect: false });
+		const session = await getSession();
+		console.log(session);
+
+		if(res){
+			router.push('/dashboard');
+		} else {
+			alert('incorrect creds');
+		}
+	}
+
+	function handleChange(event){
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+
+		if(name === 'username') setUsername(value)
+		if(name === 'password') setPassword(value)
 	}
 
 	return (
@@ -19,17 +48,17 @@ function Login() {
 					<p>OR</p>
 					<hr/>
 				</div>
-				<h3>Sign in with email</h3>
-				<form className={FormStyle.form}>
+				<h3>Sign in with credentials</h3>
+				<form className={FormStyle.form} onSubmit={credentials}>
 					<label className={FormStyle.label}>
 						Username
-						<input type="text" autoComplete="username" name="username" />
+						<input type="text" autoComplete="username" name="username" value={username} onChange={handleChange} />
 					</label>
 					<br />
 					<br />
 					<label className={FormStyle.label}>
 						Password
-						<input type="password" autoComplete="new-password" name="password" />
+						<input type="password" autoComplete="new-password" name="password" value={password} onChange={handleChange} />
 					</label>
 					<br />
 					<br />
