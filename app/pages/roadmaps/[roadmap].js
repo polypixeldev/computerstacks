644a072462b6d6d59e9696196a84b0a8560e6414
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-10-25 16:39:48 -0400

    Add basic functionality to roadmaps/[roadmap]

--- app/pages/roadmaps/[roadmap].js
+++ app/pages/roadmaps/[roadmap].js
@@ -1,14 +1,12 @@
-import { useRouter } from 'next/router';
 import HeadStyles from "../../styles/Head.module.css";
+import axios from 'axios';
 
-function Roadmap() {
-	const router = useRouter();
-	const { roadmap } = router.query;
+function Roadmap(props) {
 	return (
 		<main>
 			<section className={HeadStyles.head}>
-				<h2>{roadmap}</h2>
-				<p>(description) Lorem ipsum dolor sit amet, consectetur</p>
+				<h2>{props.data.roadmap}</h2>
+				<p>{props.data.description}</p>
 				<div className={HeadStyles.actionDiv}>
 					<p>1</p>
 					<p>2</p>
@@ -23,6 +21,50 @@ function Roadmap() {
 			</section>
 		</main>
 	)
+}
+
+export async function getStaticPaths() {
+	// For development
+	
+	return {
+		paths: [{ params: { roadmap: "yes" } }, { params: { roadmap: "hmmm" } }, { params: { roadmap: "no" } }, { params: { roadmap: "maybe" } }],
+		fallback: true
+	}
+
+	const ROADMAPS_META_URL = ``;
+	
+	let res = { paths: [], fallback: true };
+
+	const data = await axios.get(ROADMAPS_META_URL)?.data;
+
+	if(!data) return res;
+
+	for(let roadmap of data.roadmaps) res.paths.push({ params: { roadmap: roadmap.uri } })
+
+	return res;
+}
+
+export async function getStaticProps({ params }) {
+		// For development
+	
+		return {
+			props: {
+				data: {
+					description: "roadmap desc desc",
+					roadmap: Array.from(params.roadmap).reverse().join('')
+				}
+			}
+		}
+		
+		const ROADMAP_DATA_URL = ``;
+	
+		let res = { revalidate: 60, props: { data: {}, error: false } };
+	
+		let data = await axios.get(ROADMAP_DATA_URL);
+		data = data.data;
+		if(!data) res.props.error = true; else res.props.data = data;
+	
+		return res;
 }
 
 export default Roadmap;
