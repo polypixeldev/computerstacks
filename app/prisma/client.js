ac98cea7a604f7b58358e3eb53a9ea452b1a6c02
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-11-01 20:14:38 -0400

    Fix too many prisma clients instantiating in development mode

--- app/prisma/client.js
+++ app/prisma/client.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
