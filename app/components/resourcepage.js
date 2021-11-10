59b9ba2e3d3e9924aa6ab7274a60f915c47a5db9
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-11-09 20:34:09 -0500

    Favorite storing system

--- app/components/resourcepage.js
+++ app/components/resourcepage.js
@@ -1,8 +1,31 @@
 import HeadStyle from "../styles/Head.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 import styles from "../styles/Resource.module.css";
+import favorite from "../public/favorite.svg";
+import notfavorite from "../public/notfavorite.svg";
 
 function ResourcePage(props) {
+	const [isFavorite, setIsFavorite] = useState(false);
+
+	function handleFavorite() {
+		const FAVORITE_URL = `/api/user/favorite`;
+
+		if (isFavorite) {
+			setIsFavorite(false);
+			axios.post(FAVORITE_URL, {
+				uri: `${props.category}/${props.subcategory}/${props.resource}`,
+			});
+		} else {
+			setIsFavorite(true);
+			axios.post(FAVORITE_URL, {
+				uri: `${props.category}/${props.subcategory}/${props.resource}`,
+			});
+		}
+	}
+
 	return (
 		<main>
 			<section className={HeadStyle.head} id="head">
@@ -24,7 +47,13 @@ function ResourcePage(props) {
 				<p>{props.data.description}</p>
 				<div className={HeadStyle.actionDiv}>
 					<p>1</p>
-					<p>2</p>
+					<Image
+						onClick={handleFavorite}
+						src={isFavorite ? favorite : notfavorite}
+						alt="Favorite button"
+						width={75}
+						height={75}
+					/>
 					<p>3</p>
 				</div>
 			</section>
---
+++ app/pages/api/user/favorite.js
@@ -0,0 +1,26 @@
+import getDb from "../../../db/mongoose";
+import { getSession } from "next-auth/react";
+
+async function favorite(req, res) {
+	const { user } = await getDb();
+
+	const session = await getSession({ req });
+
+	if (!session) return res.status(401).end();
+
+	if (session.user.favorites.includes(req.body.uri)) {
+		await user.updateOne(
+			{ _id: session.user._id },
+			{ $pull: { favorites: req.body.uri } }
+		);
+	} else {
+		await user.updateOne(
+			{ _id: session.user._id },
+			{ $push: { favorites: req.body.uri } }
+		);
+	}
+
+	return res.status(200).end();
+}
+
+export default favorite;
