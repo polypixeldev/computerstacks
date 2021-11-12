d05f48328f842422b4d794ad2d79345c6325cd0f
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-11-11 21:31:41 -0500

    Display favorite list on dashbaord

--- app/pages/dashboard.js
+++ app/pages/dashboard.js
@@ -2,10 +2,14 @@ import styles from "../styles/Dashboard.module.css";
 import { useSession } from "next-auth/react";
 import Loading from "../components/loading";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Image from "next/image";
 import profile from "../public/profile.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
+	const [favorites, setFavorites] = useState([]);
 	const router = useRouter();
 	const { data: session, status } = useSession({
 		required: true,
@@ -14,7 +18,40 @@ function Dashboard() {
 		},
 	});
 
+	useEffect(() => {
+		if (status !== "authenticated") return;
+		console.log("e");
+
+		let newFavs = [];
+		let queries = [];
+
+		for (let fav of session.user.favorites) {
+			const uri = fav.split("/")[2];
+
+			queries.push(
+				axios.get(`/api/library/resource?uri=${uri}`).then((res) => {
+					newFavs.push({ ...res.data, uri: fav });
+				})
+			);
+		}
+
+		Promise.all(queries).then(() => {
+			setFavorites(newFavs);
+		});
+	}, [status, session?.user.favorites]);
+
 	if (status === "loading") return <Loading />;
+
+	function listFavorites() {
+		console.log(favorites);
+		return favorites.map((favorite) => (
+			<h3 key={favorite.uri}>
+				<Link href={`/library/${favorite.uri}`}>
+					<a className="link">{favorite.name}</a>
+				</Link>
+			</h3>
+		));
+	}
 
 	return (
 		<main>
@@ -32,7 +69,8 @@ function Dashboard() {
 				<h2>Roadmaps</h2>
 			</section>
 			<section className="section3">
-				<h2>Resources</h2>
+				<h2>Favorites</h2>
+				{listFavorites()}
 			</section>
 		</main>
 	);
