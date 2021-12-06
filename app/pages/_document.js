e6444988dd18f4bd2706ab5a42aca4f572bdcbd4
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-12-05 19:54:10 -0500

    Add metadata

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -4,12 +4,25 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Zilla+Slab&family=Dosis:wght@300&display=swap"
 						rel="stylesheet"
 					/>
+					<meta name="application-name" content="ComputerStacks" />
+					<meta name="author" content="Samuel Fernandez" />
+					<meta
+						name="description"
+						content="A digital collection of computer-related resources and support"
+					/>
+					<meta
+						name="keywords"
+						content="computers,programming,learn,resources"
+					/>
+					<meta name="theme-color" content="#23272a" />
+					<meta name="color-schema" content="dark light" />
+					<meta name="robots" content="index,follow" />
 				</Head>
 				<body>
 					<Main />
