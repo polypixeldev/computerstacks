7c907de7fc7b9f6fb98577dead8ff3322f7f68da
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-12-12 19:55:57 -0500

    Add link icon to resource link

--- app/components/resourcepage.js
+++ app/components/resourcepage.js
@@ -125,7 +125,52 @@ function ResourcePage(props) {
 				</h3>
 				<h2>
 					<Link href={props.data.link}>
-						<a className="link">{props.data.name}</a>
+						<a className={`link ${styles.linkA}`} target="_blank">
+							{props.data.name}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 191.65 66.467"
+								version="1.1"
+							>
+								<defs id="defs4">
+									<clipPath id="clipPath3843">
+										<path
+											id="path3845"
+											style={{ color: '#000000', fill: '#999999' }}
+											d="m-127.28-391.22v-60.411h-52.004l-46.808 13.214h-5v-8.9286c0-22.333 18.422-40.719 40.75-40.719h63.062v-11.25h-176.28v108.09z"
+										/>
+									</clipPath>
+								</defs>
+								<g id="layer1" transform="translate(-67.72 -414.1)">
+									<rect
+										id="rect3796"
+										style={{
+											color: '#000000',
+											stroke: '#000000',
+											strokeWidth: 20,
+											fill: 'none',
+										}}
+										transform="matrix(-1 0 0 -1 -27.857 0)"
+										clipPath="url(#clipPath3843)"
+										rx="23.234"
+										ry="23.234"
+										height="46.467"
+										width="109.54"
+										y="-470.57"
+										x="-277.23"
+									/>
+									<use
+										id="use3832"
+										xlinkHref="#rect3796"
+										transform="matrix(-1 0 0 -1 327.09 894.66)"
+										height="1052.3622"
+										width="744.09448"
+										y="0"
+										x="0"
+									/>
+								</g>
+							</svg>
+						</a>
 					</Link>
 				</h2>
 				<p>{props.data.description}</p>
