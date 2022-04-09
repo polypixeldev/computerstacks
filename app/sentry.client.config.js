330131451f16554fac72f39a8877f80fea83992e
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2022-04-09 16:07:38 -0400

    Use Sentry DSN from environment

--- app/sentry.client.config.js
+++ app/sentry.client.config.js
@@ -4,10 +4,8 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://885760b140e14df1aba9dbef33ab7bfe@o920118.ingest.sentry.io/6101182',
+  dsn: process.env.SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
