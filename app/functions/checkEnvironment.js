28c137866d494b3e619a4858bf254dc45ef7ce99
Poly-Pixel <79737178+Poly-Pixel@users.noreply.github.com>
2022-04-19 18:59:04 -0400

    Skip environment check in testing environment

--- app/functions/checkEnvironment.js
+++ app/functions/checkEnvironment.js
@@ -1,6 +1,10 @@
 const environmentVariables = ["NEXTAUTH_URL", "GOOGLE_ID", "GOOGLE_SECRET", "GITHUB_ID", "GITHUB_SECRET", "EMAIL_SERVER", "EMAIL_FROM", "MONGODB_URI", "NODE_ENV", "JWT_SIGNING_PRIVATE_KEY", "NEXTAUTH_SECRET", "JWT_ENCRYPTION_KEY", "SENTRY_DSN"];
 
 function checkEnvironment() {
+	if(process.env.NODE_ENV === 'testing') {
+		return [];
+	}
+	
 	const missing = [];
 
 	for (const variable of environmentVariables) {
